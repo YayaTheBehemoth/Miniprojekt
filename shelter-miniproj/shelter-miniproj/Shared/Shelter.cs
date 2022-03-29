a261e85b6087edd349ac42fdd12e7703bbd955d5
YayaTheBehemoth <allanermansur@gmail.com>
2022-03-29 09:21:05 +0200

    initial commit hihi :3

---
+++ shelter-miniproj/shelter-miniproj/Shared/Shelter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace shelterminiproj.Shared
+{
+    public class Shelter
+    {
+        public string name { get; set; }
+        public int tilladte_personer { get; set; }
+        public bool tilgængelig { get; set; }
+        public Shelter(string n, int tp, bool t)
+        {
+            name = n;
+            tilladte_personer = tp;
+            tilgængelig = t;
+        }
+    }
+}
