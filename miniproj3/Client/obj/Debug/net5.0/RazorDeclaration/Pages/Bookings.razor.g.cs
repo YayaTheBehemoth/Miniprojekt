ee2c3920a0b6031d62350f0c6c6504f23b573610
RoachSilverback <95019847+RoachSilverback@users.noreply.github.com>
2022-03-31 12:59:05 +0200

    Mjah

--- miniproj3/Client/obj/Debug/net5.0/RazorDeclaration/Pages/Bookings.razor.g.cs
+++ miniproj3/Client/obj/Debug/net5.0/RazorDeclaration/Pages/Bookings.razor.g.cs
@@ -13,84 +13,84 @@ namespace miniproj3.Client.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/Users/placeholder/Projects/miniproj3/miniproj3/Client/_Imports.razor"
+#line 1 "C:\Users\zulud\OneDrive\Dokumenter\Skole\Fag\2. semester\Miniprojekt\Miniprojekt\miniproj3\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/placeholder/Projects/miniproj3/miniproj3/Client/_Imports.razor"
+#line 2 "C:\Users\zulud\OneDrive\Dokumenter\Skole\Fag\2. semester\Miniprojekt\Miniprojekt\miniproj3\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/placeholder/Projects/miniproj3/miniproj3/Client/_Imports.razor"
+#line 3 "C:\Users\zulud\OneDrive\Dokumenter\Skole\Fag\2. semester\Miniprojekt\Miniprojekt\miniproj3\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/placeholder/Projects/miniproj3/miniproj3/Client/_Imports.razor"
+#line 4 "C:\Users\zulud\OneDrive\Dokumenter\Skole\Fag\2. semester\Miniprojekt\Miniprojekt\miniproj3\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/placeholder/Projects/miniproj3/miniproj3/Client/_Imports.razor"
+#line 5 "C:\Users\zulud\OneDrive\Dokumenter\Skole\Fag\2. semester\Miniprojekt\Miniprojekt\miniproj3\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/placeholder/Projects/miniproj3/miniproj3/Client/_Imports.razor"
+#line 6 "C:\Users\zulud\OneDrive\Dokumenter\Skole\Fag\2. semester\Miniprojekt\Miniprojekt\miniproj3\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/Users/placeholder/Projects/miniproj3/miniproj3/Client/_Imports.razor"
+#line 7 "C:\Users\zulud\OneDrive\Dokumenter\Skole\Fag\2. semester\Miniprojekt\Miniprojekt\miniproj3\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/Users/placeholder/Projects/miniproj3/miniproj3/Client/_Imports.razor"
+#line 8 "C:\Users\zulud\OneDrive\Dokumenter\Skole\Fag\2. semester\Miniprojekt\Miniprojekt\miniproj3\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/Users/placeholder/Projects/miniproj3/miniproj3/Client/_Imports.razor"
+#line 9 "C:\Users\zulud\OneDrive\Dokumenter\Skole\Fag\2. semester\Miniprojekt\Miniprojekt\miniproj3\Client\_Imports.razor"
 using miniproj3.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/Users/placeholder/Projects/miniproj3/miniproj3/Client/_Imports.razor"
+#line 10 "C:\Users\zulud\OneDrive\Dokumenter\Skole\Fag\2. semester\Miniprojekt\Miniprojekt\miniproj3\Client\_Imports.razor"
 using miniproj3.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "/Users/placeholder/Projects/miniproj3/miniproj3/Client/_Imports.razor"
+#line 11 "C:\Users\zulud\OneDrive\Dokumenter\Skole\Fag\2. semester\Miniprojekt\Miniprojekt\miniproj3\Client\_Imports.razor"
 using Syncfusion;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/placeholder/Projects/miniproj3/miniproj3/Client/Pages/Bookings.razor"
+#line 2 "C:\Users\zulud\OneDrive\Dokumenter\Skole\Fag\2. semester\Miniprojekt\Miniprojekt\miniproj3\Client\Pages\Bookings.razor"
 using miniproj3.Shared;
 
 #line default
@@ -105,7 +105,7 @@ using miniproj3.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 65 "/Users/placeholder/Projects/miniproj3/miniproj3/Client/Pages/Bookings.razor"
+#line 65 "C:\Users\zulud\OneDrive\Dokumenter\Skole\Fag\2. semester\Miniprojekt\Miniprojekt\miniproj3\Client\Pages\Bookings.razor"
        
     private Booking[] shelters;
     
